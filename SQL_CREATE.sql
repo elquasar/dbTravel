acfae44539f5f4f9796b57975ba45e883e588e78
Quentin Leprat <quentin.leprat@etu.utc.fr>
2020-05-30 16:07:26 +0200

    Update SQL_CREATE.sql

--- SQL_CREATE.sql
+++ SQL_CREATE.sql
@@ -1,1 +1,27 @@
-vdcfd
+CREATE TABLE RessourceHumaine (
+        
+        NSS text PRIMARY KEY,
+        nom text NOT NULL,
+        prenom text,
+        date_naissance date
+);
+
+CREATE TABLE Personnel (
+        
+        NSS text PRIMARY KEY,
+        poste text NOT NULL,
+        salaire decimal NOT NULL,
+        FOREIGN KEY (NSS) REFERENCES RessourceHumaine(NSS)
+);
+
+CREATE TABLE Client (
+        
+        NSS text PRIMARY KEY,
+        num_tel int,
+        adresse text NOT NULL
+        FOREIGN KEY (NSS) REFERENCES RessourceHumaine(NSS)
+);
+
+CREATE TABLE Societe (
+        
+);
