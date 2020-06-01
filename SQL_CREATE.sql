701b4f20758e44b19ac5b8ab341eceb8a90688f1
Quentin Leprat <quentin.leprat@etu.utc.fr>
2020-06-01 17:26:20 +0200

    Update SQL_CREATE.sql

--- SQL_CREATE.sql
+++ SQL_CREATE.sql
@@ -129,3 +129,21 @@ CREATE TABLE Notation (
     FOREIGN KEY (Client) REFERENCES Client(NSS),
     FOREIGN KEY (CircuitTouristique) REFERENCES CircuitTouristique(id)
 );
+
+
+
+INSERT INTO RessourceHumaine VALUES ('198072722924031','Leprat','Quentin','1998-07-01');
+INSERT INTO RessourceHumaine VALUES ('198074722520893','Durand','Antoine','2007-09-01');
+INSERT INTO RessourceHumaine VALUES ('598072452892409','Renard','Vincent','1998-08-05');
+
+
+INSERT INTO Client VALUES ('198074722520893','0689784565','Boulevard de la république');
+INSERT INTO Client VALUES ('198074722520893','689784565','Rue des belles femmes');
+INSERT INTO Personnel VALUES ('198072722924031','Charge d affaire','5000',NULL);
+
+INSERT INTO CircuitTouristique VALUES ('01','Aquatique','2020-05-30','7 jours','25',5);
+INSERT INTO CircuitTouristique VALUES ('02','Montagne','2020-02-12','15 jours','18',3);
+INSERT INTO CircuitTouristique VALUES ('03,'Aviation','2020-07-30','7 jours','10',1);
+
+
+
